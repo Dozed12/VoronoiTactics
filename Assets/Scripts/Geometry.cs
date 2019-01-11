16b58bca31c2643caab9bbe8e5d241436cab14d5
Dozed12 <up201607929@fe.up.pt>
2019-01-11 02:03:16 +0000

    Faster Inside for Convex; Inside Concave

--- Assets/Scripts/Geometry.cs
+++ Assets/Scripts/Geometry.cs
@@ -10,33 +10,66 @@ using VoronoiLib.Structures;
 public static class Geometry
 {
 
-    //Useful point struct
-    private struct Point
+    public static bool InsideConvex(VPoint testPoint, List<VPoint> polygon)
     {
-        public short x;
-        public short y;
-        public Point(short aX, short aY) { x = aX; y = aY; }
-        public Point(int aX, int aY) : this((short)aX, (short)aY) { }
-    }
+        //Check if a triangle or higher n-gon
+        Debug.Assert(polygon.Count >= 3);
 
-    //Point inside convex polygon
-    public static bool InsideConvex(List<VPoint> vertices, VPoint point)
-    {
-        VPoint[] poly = vertices.ToArray();
-        var coef = poly.Skip(1).Select((p, i) =>
-                                           (point.Y - poly[i].Y) * (p.X - poly[i].X)
-                                         - (point.X - poly[i].X) * (p.Y - poly[i].Y))
-                                   .ToList();
+        //n>2 Keep track of cross product sign changes
+        var pos = 0;
+        var neg = 0;
 
-        if (coef.Any(p => p == 0))
-            return true;
-
-        for (int i = 1; i < coef.Count(); i++)
+        for (var i = 0; i < polygon.Count; i++)
         {
-            if (coef[i] * coef[i - 1] < 0)
+            //If point is in the polygon
+            if (polygon[i] == testPoint)
+                return true;
+
+            //Form a segment between the i'th point
+            var x1 = polygon[i].X;
+            var y1 = polygon[i].Y;
+
+            //And the i+1'th, or if i is the last, with the first point
+            var i2 = i < polygon.Count - 1 ? i + 1 : 0;
+
+            var x2 = polygon[i2].X;
+            var y2 = polygon[i2].Y;
+
+            var x = testPoint.X;
+            var y = testPoint.Y;
+
+            //Compute the cross product
+            var d = (x - x1) * (y2 - y1) - (y - y1) * (x2 - x1);
+
+            if (d > 0) pos++;
+            if (d < 0) neg++;
+
+            //If the sign changes, then point is outside
+            if (pos > 0 && neg > 0)
                 return false;
         }
+
+        //If no change in direction, then on same side of all segments, and thus inside
         return true;
+    }
+
+    //Point inside concave polygon
+    public static bool InsideConcave(List<VPoint> vertices, VPoint point)
+    {
+        bool result = false;
+        int j = vertices.Count() - 1;
+        for (int i = 0; i < vertices.Count(); i++)
+        {
+            if (vertices[i].Y < point.Y && vertices[j].Y >= point.Y || vertices[j].Y < point.Y && vertices[i].Y >= point.Y)
+            {
+                if (vertices[i].X + (point.Y - vertices[i].Y) / (vertices[j].Y - vertices[i].Y) * (vertices[j].X - vertices[i].X) < point.X)
+                {
+                    result = !result;
+                }
+            }
+            j = i;
+        }
+        return result;
     }
 
 }
