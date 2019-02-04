9e06b9e92efd903c4955888b39e4d403b70f3840
Dozed12 <up201607929@fe.up.pt>
2019-02-04 00:30:15 +0000

    Multithreaded HeightNoiseMapTexture

--- Assets/Scripts/Map.cs
+++ Assets/Scripts/Map.cs
@@ -649,7 +649,8 @@ public class MapData
 
             //Height Greyscale (Quite simple so doesnt need PixelMatrix)
             Color[] pixels = new Color[settings.WIDTH * settings.HEIGHT];
-            for (int i = 0; i < settings.WIDTH; i++)
+            //Multithreaded
+            Parallel.For(0, settings.WIDTH, i =>
             {
                 int finalI = i * settings.WIDTH;
                 for (int j = 0; j < settings.HEIGHT; j++)
@@ -657,7 +658,7 @@ public class MapData
                     float val = geography.HEIGHTMAP[i, j];
                     pixels[finalI + j] = new Color(val, val, val);
                 }
-            }
+            });
 
             //Create texture
             Texture2D texture = new Texture2D(settings.WIDTH, settings.HEIGHT);
