aeca63479022c1bca81e50a37b11454943c533e8
Dozed12 <up201607929@fe.up.pt>
2019-02-06 18:20:14 +0000

    Reduce Grassland Tree Chance

--- Assets/Scripts/Map.cs
+++ Assets/Scripts/Map.cs
@@ -302,6 +302,7 @@ public class MapData
         //TODO Should be set elsewhere
         //TODO Wont need so many octaves considering we dont use all the detail, but maybe we will
         fastnoise.SetNoiseType(FastNoise.NoiseType.SimplexFractal);
+        fastnoise.SetFractalType(FastNoise.FractalType.FBM);
         fastnoise.SetFractalOctaves(3);
         fastnoise.SetFractalLacunarity(2.0f);
         fastnoise.SetFrequency(1);
