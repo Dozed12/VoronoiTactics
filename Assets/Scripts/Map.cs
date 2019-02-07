02ef17151e241745bd63dc06bc73259db9f8d439
Dozed12 <up201607929@fe.up.pt>
2019-02-07 17:34:23 +0000

    Revert "Separate octaves for Height and Terrain (Terrain can be lower)"
    This reverts commit 832f75778e6aa9dd9f936ebcabc7b5f964914db0.

--- Assets/Scripts/Map.cs
+++ Assets/Scripts/Map.cs
@@ -303,18 +303,14 @@ public class MapData
         //TODO Wont need so many octaves considering we dont use all the detail, but maybe we will
         fastnoise.SetNoiseType(FastNoise.NoiseType.SimplexFractal);
         fastnoise.SetFractalType(FastNoise.FractalType.FBM);
+        fastnoise.SetFractalOctaves(6);
         fastnoise.SetFractalLacunarity(2.0f);
         fastnoise.SetFrequency(1);
 
         //Noise block size
-        //TODO Settings probably better in other place
+        //TODO probably better in other place
         int terrainBlockSize = 10;
-        int heightBlockSize = 2;
-
-        //Octaves
-        //TODO Settings probably better in other place
-        int heightOctaves = 6;
-        int terrainOctaves = 2;
+        int heightBlockSize = 1;
 
         //Noise lookups
         HeightMap();
@@ -326,9 +322,6 @@ public class MapData
 
             //New seed
             fastnoise.SetSeed(UnityEngine.Random.Range(Int16.MinValue, Int16.MaxValue));
-
-            //Octaves
-            fastnoise.SetFractalOctaves(heightOctaves);
 
             //Biome height frequency
             fastnoise.SetFrequency(biome.heightFreq);
@@ -380,9 +373,6 @@ public class MapData
 
             //New seed
             fastnoise.SetSeed(UnityEngine.Random.Range(Int16.MinValue, Int16.MaxValue));
-
-            //Octaves
-            fastnoise.SetFractalOctaves(terrainOctaves);
 
             //Biome terrain frequency
             fastnoise.SetFrequency(biome.terrainFreq);
