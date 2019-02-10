8de6f6fee81e8dbb380378ee15e8585ffe18c7f1
Dozed12 <up201607929@fe.up.pt>
2019-02-10 04:17:02 +0000

    Map Preview

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -11,21 +11,40 @@ public class UIManager : MonoBehaviour
     public GameObject BattleSetupUI;
     public GameObject BattleUI;
     public GameObject MainUI;
-    
+
     //========================================
 
     //Main UI Group TODO do missing
-    public void OnRandomBattle(){
+    public void OnRandomBattle()
+    {
         MainUI.SetActive(false);
         BattleSetupUI.SetActive(true);
     }
 
     //========================================
 
+    public GameObject MapPreview;
+
     //Battle Setup UI Group TODO do missing
-    public void OnBattle(){
+    public void OnGenerate()
+    {
+        //Generate
+        Map.Generate();
+
+        //Change preview image
+        Sprite sprite = Sprite.Create(Map.mapData.mapModes["Map"], new Rect(0, 0, Map.mapData.settings.WIDTH, Map.mapData.settings.HEIGHT), new Vector2(0.5f, 0.5f), 100, 0, SpriteMeshType.FullRect);
+        MapPreview.GetComponent<Image>().sprite = sprite;
+    }
+
+    //Battle Setup UI Group TODO do missing
+    public void OnBattle()
+    {
         BattleSetupUI.SetActive(false);
         BattleUI.SetActive(true);
     }
+
+    //========================================
+
+    public Map Map;
 
 }
