01787494f3c95ccdb8c6d6ed14a8cfa03620d167
Dozed12 <up201607929@fe.up.pt>
2019-02-10 01:00:53 +0000

    Some menu flow

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+
+    //Major UIs
+    public GameObject battleSetupUI;
+    public GameObject battleUI;
+    public GameObject mainUI;
+    
+    //========================================
+
+    //Main UI Group TODO do missing
+    public void OnRandomBattle(){
+        mainUI.SetActive(false);
+        battleSetupUI.SetActive(true);
+    }
+
+}
