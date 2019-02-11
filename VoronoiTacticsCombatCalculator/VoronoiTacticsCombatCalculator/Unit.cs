a976e5538a0082ec9756fffe5ad2e6f5181643a9
Dozed12 <up201607929@fe.up.pt>
2019-02-11 22:15:30 +0000

    Unit class

---
+++ VoronoiTacticsCombatCalculator/VoronoiTacticsCombatCalculator/Unit.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VoronoiTacticsCombatCalculator
+{
+    class Unit
+    {
+
+        //Unit data
+
+        public int number;
+        public float morale;
+        public float moraleRecover;
+        public float speed;
+        public float fatigueModifier;
+
+        public bool ranged;
+        public float rangedAccuracy;
+        public float rangedAttack;
+        public int rangedTargets;
+        public int rangedReload;
+
+        public bool melee;
+        public float meleeAttack;
+        public int meleeTime;
+
+        public float rangedDefense;
+        public float meleeDefense;
+
+        public bool charge;
+        public float chargeAttack;
+
+        //Terrain data (Just for test here)
+
+    }
+}
